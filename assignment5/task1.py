8be0b194cf7845c1c6f6305e41a6b5ed42bc2ecb
Akash Sarkar <as173533@gmail.com>
2026-02-02 13:23:42 +0530

    space added

--- assignment5/task1.py
+++ assignment5/task1.py
@@ -9,4 +9,4 @@ name = input("Enter the student's name: ").strip().lower()
 if name in students_marks:
     print(f"{name}'s marks: {students_marks[name]}")
 else:
-    print("Student not found.")
+    print("Student not found.") 
